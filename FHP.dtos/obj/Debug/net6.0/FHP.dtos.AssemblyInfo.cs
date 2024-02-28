69a67e4807b22d6e574a8092d1cb923302ebf356
Taoqeer <mohdtaoqeer404@gail.com>
2024-02-28 14:42:53 +0530

    hh

--- FHP.dtos/obj/Debug/net6.0/FHP.dtos.AssemblyInfo.cs
+++ FHP.dtos/obj/Debug/net6.0/FHP.dtos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FHP.dtos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb3422d2e63625544c8ad14613e1c818b9548da")]
 [assembly: System.Reflection.AssemblyProductAttribute("FHP.dtos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FHP.dtos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
