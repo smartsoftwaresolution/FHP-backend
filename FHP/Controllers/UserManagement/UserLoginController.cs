fc34a83b8cd90ff3e8bac5db9440c10aafec382d
ZubairKhanSft <zubair.softw@gmail.com>
2024-03-11 11:19:14 +0530

    crocin

--- FHP/Controllers/UserManagement/UserLoginController.cs
+++ FHP/Controllers/UserManagement/UserLoginController.cs
@@ -364,7 +364,7 @@ namespace FHP.Controllers.UserManagement
                 return BadRequest(ModelState.GetErrorList());
             }
 
-            var response = new BaseResponseAdd();
+            var response = new BaseResponse<object>();
             try
             {
                 var exist = await _manager.GetUserByEmail(email);
@@ -377,6 +377,7 @@ namespace FHP.Controllers.UserManagement
 
                 if(exist.Otp == otp)
                 {
+                    response.Data = exist.Id;
                     response.StatusCode = 200;
                     response.Message = $"otp matched successfully!!";
                     return Ok(response);
