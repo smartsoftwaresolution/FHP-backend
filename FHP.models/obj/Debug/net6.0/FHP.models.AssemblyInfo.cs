d8f907c01702173b91d7f98d6db3a08496cd97a6
Taoqeer <mohdtaoqeer404@gmail.com>
2024-04-15 11:13:45 +0530

    after pull

--- FHP.models/obj/Debug/net6.0/FHP.models.AssemblyInfo.cs
+++ FHP.models/obj/Debug/net6.0/FHP.models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FHP.models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050ecd34340c96d8637a058847987130e6d49be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FHP.models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FHP.models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
