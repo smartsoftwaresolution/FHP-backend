ca8189d07c44593adad6703c358d8fbc2272b995
Taoqeer <mohdtaoqeer404@gmail.com>
2024-04-18 14:40:33 +0530

    swa

--- FHP.factories/obj/Debug/net6.0/FHP.factories.AssemblyInfo.cs
+++ FHP.factories/obj/Debug/net6.0/FHP.factories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FHP.factories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b11087975712bf99ba4b962552bc42755f0b1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf0a48c153f7caa17f3045ca5f4ae83fb70acac")]
 [assembly: System.Reflection.AssemblyProductAttribute("FHP.factories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FHP.factories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
