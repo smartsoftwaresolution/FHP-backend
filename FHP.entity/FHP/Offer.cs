1a80c4a8148a284c84319dd0cc554b95407d9b80
ZubairKhanSft <zubair.softw@gmail.com>
2024-04-23 16:14:28 +0530

    changes

--- FHP.entity/FHP/Offer.cs
+++ FHP.entity/FHP/Offer.cs
@@ -15,7 +15,6 @@ namespace FHP.entity.FHP
         public int EmployerId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public bool IsAccepted { get; set; }
         public Constants.RecordStatus Status { get; set; }
         public Constants.OfferStatus IsAvaliable { get; set; }
         public string CancelReason { get; set; }
--- FHP.manager/obj/Debug/net6.0/FHP.manager.AssemblyInfo.cs
+++ FHP.manager/obj/Debug/net6.0/FHP.manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FHP.manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074fb42e8e28ef2341887b852e31491f31bde56d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FHP.manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FHP.manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
