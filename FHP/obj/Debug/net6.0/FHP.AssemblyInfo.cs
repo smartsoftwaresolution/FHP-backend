321fbe08ece15251b42635318c6626f4b30d2b3c
ZubairKhanSft <zubair.softw@gmail.com>
2024-05-16 16:14:46 +0530

    hrhr

--- FHP/obj/Debug/net6.0/FHP.AssemblyInfo.cs
+++ FHP/obj/Debug/net6.0/FHP.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FHP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1fe2efc992fb7c8041dbe1506b79b917e9b04f")]
->>>>>>> 326fc653058e6e39a88e5c3d6cbb59221a1d66cd
 [assembly: System.Reflection.AssemblyProductAttribute("FHP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FHP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
